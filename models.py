0d24d473c76087d1959289e07f11199528c37cb9
Shruti <shruti.uppin@target.com>
2021-03-01 19:19:27 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, create_engine
+from sqlalchemy import Column, String, Integer, create_engine, DateTime
 from flask_sqlalchemy import SQLAlchemy
 import json
 import os
@@ -27,19 +27,62 @@ Have title and release year
 '''
 
 
-class Person(db.Model):
-  __tablename__ = 'Peoples'
+class Movie(db.Model):
+    __tablename__ = 'movies'
 
-  id = Column(String, primary_key=True)
-  name = Column(String)
-  catchphrase = Column(String)
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+    release_date = Column(DateTime, nullable=False)
 
-  def __init__(self, name, catchphrase=""):
-    self.name = name
-    self.catchphrase = catchphrase
+    def __init__(self, title, release_date):
+        self.title = title
+        self.release_date = release_date
 
-  def format(self):
-    return {
-        'id': self.id,
-        'name': self.name,
-        'catchphrase': self.catchphrase}
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+
+    def format(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'release_date': self.release_date}
+
+
+class Actor(db.Model):
+    __tablename__ = 'actors'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    age = Column(String, nullable=False)
+    gender = Column(String, nullable=False)
+
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender=gender
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'name': self.name,
+            'age': self.age,
+            'gender': self.gender}
